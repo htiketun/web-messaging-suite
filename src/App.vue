02750c2d7d8ff2e97f82ae594f76156e69b5c8f6
htiketun <kohtiketun@outlook.com>
2024-07-19 17:39:26 +0000

    Update theme settings

--- src/App.vue
+++ src/App.vue
@@ -10,3 +10,4 @@ provide('mediaUrl', 'http://127.0.0.1:8888/');
 provide('socketUrl', 'ws://localhost:8000/ws/');
 </script>
 <!-- Commit 6: 2024-07-12T16:59:16 -->
+<!-- Commit 9: 2024-07-19T17:39:26 -->
